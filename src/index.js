5fe4d91f9cab66744566f5dacee01b21cea46f1c
Vlamir Santo <vlamirsanto@gmail.com>
2018-02-28 19:05:29 -0300

    - Customização da tela de welcome; - Criação das rotas.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+
+import Routes from 'routes';
+
+const App = () => (
+  <Routes />
+)
+
+export default App;
---
+++ src/pages/welcome/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+const Welcome = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continuar, precisamos que você informe seu usuário no GitHub.
+    </Text>
+
+    <View style={styles.form}>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="rgba(0,0,0,0)"
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {}}
+      >
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+export default Welcome;
